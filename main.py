e3fffa3c73e7d97e710170eb92850e8fae9b685e
Grunzolyth <WunderlichAlex96@googlemail.com>
2025-07-18 16:27:52 +0200

    Start of Project with a couple of constants

---
+++ main.py
@@ -0,0 +1,16 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+# import the connect_database function
+# and the database_version variable
+# from database.py into the current file
+from database import connect_database, database_version
+
+
+def main():
+    print("Starting Asteroids!" )
+
+
+if __name__ == "__main__":
+    main()
